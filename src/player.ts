5f9ae08ef55e8561ee4c4694cfda8a2b091244c0
Jan Steikowski <j.steikowski@aol.com>
2023-07-28 15:56:32 +0700

    Restructured placeships to player class

--- src/player.ts
+++ src/player.ts
@@ -1,6 +1,6 @@
-'use strict'
-import { Gameboard } from "./gameboard"
-
+"use strict";
+import { Gameboard } from "./gameboard";
+import { Ship } from "./ship";
 export class Player {
   private gameBoard: Gameboard;
   private playerName: string;
@@ -17,15 +17,36 @@ export class Player {
     return this.gameBoard;
   }
 
+  placeAllShips() {
+    const carrier: Ship = new Ship(5);
+    const battleship: Ship = new Ship(4);
+    const cruiser: Ship = new Ship(3);
+    const submarine: Ship = new Ship(3);
+    const destroyer: Ship = new Ship(2);
+    const shipsToPlace: Ship[] = [carrier, battleship, cruiser, submarine, destroyer];
+
+    this.board.placeShip(carrier, "Horizontal", [0, 0]);
+    this.board.placeShip(battleship, "Vertical", [9, 9]);
+    this.board.placeShip(cruiser, "Horizontal", [2, 0]);
+    this.board.placeShip(submarine, "Horizontal", [4, 0]);
+    this.board.placeShip(destroyer, "Horizontal", [6, 0]);
+
+    /*for (let i = 0; i < shipsToPlace.length; i++) {
+    let shipPlaced: undefined | Error;
+    do {
+      shipPlaced = player.board.placeShip(shipsToPlace[i],)
+    }
+  }*/
+  }
+
   makeMove(coords: number[] | null, player: Player) {
     if (!coords) {
       let computerResponse;
-      do { 
+      do {
         let row = Math.floor(Math.random() * 10);
         let column = Math.floor(Math.random() * 10);
         computerResponse = player.board.receiveAttack([row, column]);
-      }
-      while (computerResponse != undefined)
+      } while (computerResponse != undefined);
       return true;
     }
     let playerResponse = player.board.receiveAttack(coords);
