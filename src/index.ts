ee5be74b6f56541db4104ea439746e635dec8a58
Jan Steikowski <j.steikowski@aol.com>
2023-07-28 15:49:17 +0700

    Add clicking on grids and playing the game

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,6 @@ import { createPlayerDiv } from "./pageLoad";
 import { startNewGame } from "./game";
 const gameDiv = document.querySelector("#gameDiv");
 const game = startNewGame();
-gameDiv.appendChild(createPlayerDiv("player", game.player));
+gameDiv.appendChild(createPlayerDiv("player", game.player, game));
 
-gameDiv.appendChild(createPlayerDiv("computer", game.computer));
+gameDiv.appendChild(createPlayerDiv("computer", game.computer, game));
--- src/pageLoad.ts
+++ src/pageLoad.ts
@@ -2,15 +2,16 @@
 
 import { Player } from "./player";
 import Duck from "./img/duck.jpg";
-export function createPlayerDiv(id: string, player: Player): HTMLDivElement {
+import { Game } from "./game";
+export function createPlayerDiv(id: string, player: Player, game: Game): HTMLDivElement {
   const div: HTMLDivElement = document.createElement("div");
   div.classList.add("flex", "flex-col", "items-center", "w-full", "p-2", "rounded-xl", "border-4", "border-black");
   div.id = id;
-  div.appendChild(createGrid(player));
+  div.appendChild(createGrid(player, game));
   return div;
 }
 
-function createGrid(player: Player): HTMLDivElement {
+function createGrid(player: Player, game: Game): HTMLDivElement {
   const div: HTMLDivElement = document.createElement("div");
   div.classList.add("battleGrid");
   for (let i = 0; i < player.board.board.length; i++) {
@@ -31,10 +32,40 @@ function createGrid(player: Player): HTMLDivElement {
           }
         }
       } else {
-        gridPoint.classList.add('bg-white');
+        gridPoint.classList.add("bg-white");
+        gridPoint.addEventListener("click", function revealItem() {
+          player.board.receiveAttack([i, j]);
+          player.makeMove(null, game.player);
+          reloadGrids(game);
+          gridPoint.removeEventListener("click", revealItem);
+        });
       }
+
       div.appendChild(gridPoint);
     }
   }
   return div;
+}
+
+function reloadGrids(game: Game) {
+  const playerGrid = document.querySelector("#player").querySelector("div");
+  const computerGrid = document.querySelector("#computer").querySelector("div");
+  iterateThroughGrids(computerGrid, game.computer);
+  iterateThroughGrids(playerGrid, game.player);
+}
+
+function iterateThroughGrids(grid: HTMLDivElement, player: Player) {
+  console.log(player.board.allSunk());
+  for (let i = 0; i < grid.children.length; i++) {
+    //if (grid.children[i].classList.contains("revealed")) {
+      const row = Math.floor(i / 10);
+      const col = i % 10;
+      if (player.board.board[row][col] === "Hit") {
+        grid.children[i].classList.add("bg-red-300");
+      }
+      if (player.board.board[row][col] === "Sea") {
+        grid.children[i].classList.add("bg-blue-300");
+      }
+    //}
+  }
 }
