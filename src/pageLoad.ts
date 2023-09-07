cc02f6dddccb64995e9426891032ac10587d8f3f
Jan Steikowski <j.steikowski@aol.com>
2023-09-07 15:11:20 +0700

    Add responsive height and play button styling

--- src/pageLoad.ts
+++ src/pageLoad.ts
@@ -8,7 +8,7 @@ export function loadBGImg() {
   main.appendChild(img);
   img.id = 'bgIMG';
 }
-
+// Function to write the Introduction test 
 export function writeIntroduction() {
   // TODO: font color change to css
   const div: HTMLDivElement = document.createElement('div');
@@ -18,17 +18,27 @@ export function writeIntroduction() {
     'xl:flex-row', 'items-center', 'xl:justify-between', 'xl:gap-10');
   const introParagraph: HTMLParagraphElement = document.createElement("p");
   introParagraph.innerText =
-    "Are you ready for an epic battleship fight? Destroy all of the enemy ships before your enemy destroys yours to win. Sounds easy? Well, try it out captain, press the play button and place your ships!";
-  introParagraph.classList.add('font-bold')
+    "Are you ready for an epic battleship fight? Destroy all of the enemy ships before your enemy destroys yours to win. Sounds easy? Well, try it out captain and press the play button!";
+  introParagraph.classList.add('font-bold', 'h-fit-content', 'sm:h-16', 'xl:h-6')
+  introParagraph.id = 'introPar';
   div.appendChild(introParagraph);
   const playButton: HTMLButtonElement = document.createElement('button');
   playButton.innerText = 'Play';
   playButton.classList.add('rounded-lg', 'text-black', 'bg-white',
     'w-16', 'font-bold', 'border-2', 'border-black', 'drop-shadow-md',
     'hover:bg-gray-200', 'active:bg-gray-400');
+  playButton.addEventListener('click', () => {
+    changeIntroText(introParagraph);
+    loadBoards();
+  })
   div.appendChild(playButton);
+
 }
 
-function enemyBoard() {
+function changeIntroText(paragraph: HTMLParagraphElement) {
+  paragraph.innerText = 'Place your ships on your Board. Choose wisely! Good luck captain!';
+}
+
+function loadBoards() {
 
 }
