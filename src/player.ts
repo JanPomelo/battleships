0ab2fd64d1685e3bf451f294b0c3c30172edb60d
Jan Steikowski <j.steikowski@aol.com>
2023-09-25 14:31:55 +0700

    manually deleted unnessesary changes

--- src/player.ts
+++ src/player.ts
@@ -10,41 +10,8 @@ export class Player {
     this.name = name;
   }
 
-  get name() {
-    return this.playerName;
-  }
   get board() {
     return this.gameBoard;
-  }
-
-  placeAllShips() {
-    const carrier: Ship = new Ship(5);
-    const battleship: Ship = new Ship(4);
-    const cruiser: Ship = new Ship(3);
-    const submarine: Ship = new Ship(3);
-    const destroyer: Ship = new Ship(2);
-    const shipsToPlace: Ship[] = [carrier, battleship, cruiser, submarine, destroyer];
-
-    /*this.board.placeShip(carrier, "Horizontal", [0, 0]);
-    this.board.placeShip(battleship, "Vertical", [9, 9]);
-    this.board.placeShip(cruiser, "Horizontal", [2, 0]);
-    this.board.placeShip(submarine, "Horizontal", [4, 0]);
-    this.board.placeShip(destroyer, "Horizontal", [6, 0]);
-    */
-    for (let i = 0; i < shipsToPlace.length; i++) {
-      let shipPlaced: undefined | Error;
-      do {
-        const horVer = Math.floor(Math.random() * 2);
-        let dir: "Horizontal" | "Vertical" = "Horizontal"
-        if (horVer === 1) {
-          dir = "Vertical";
-        }
-        shipPlaced = this.board.placeShip(shipsToPlace[i], dir, [
-          Math.floor(Math.random() * 10),
-          Math.floor(Math.random() * 10),
-        ]);
-      } while (shipPlaced);
-    }
   }
 
   makeMove(coords: number[] | null, player: Player) {
